972cd3f85f6832bf2fb83f59bb781d91d4fc2f8d
chubbyhippo <58356115+chubbyhippo@users.noreply.github.com>
2024-05-25 11:33:07 +0700

    Update and rename install.sh to install-bash.sh

--- install-bash.sh
+++ install-bash.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/env  sh
 curl https://mise.run | sh
+echo 'eval "$(~/.local/bin/mise activate bash)"' >> ~/.bashrc
+eval "$(~/.local/bin/mise activate bash)"
 curl -s https://raw.githubusercontent.com/chubbyhippo/mise/main/mise.sh | /usr/bin/env sh
 
